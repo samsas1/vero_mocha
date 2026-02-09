c8a6853ce095009db774218aab2463831d44417e
Adomas <adamsas8@gmail.com>
2026-02-09 13:02:08 +0000

    add item status description

--- core/src/main/java/com/coffee/item/entity/ItemStatus.java
+++ core/src/main/java/com/coffee/item/entity/ItemStatus.java
@@ -1,5 +1,9 @@
 package com.coffee.item.entity;
 
+/**
+ * ItemStatus is an enum that represents the availability of an item.
+ * When ACTIVE, it can be ordered by customers. When INACTIVE, it is only maintained for past orders.
+ */
 public enum ItemStatus {
 
     ACTIVE,
