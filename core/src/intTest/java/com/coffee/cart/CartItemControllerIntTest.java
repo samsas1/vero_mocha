a4bcbd01d555061e23fb69740a568df157338923
Adomas <adamsas8@gmail.com>
2026-02-10 20:39:08 +0000

    wire up cart item controller

---
+++ core/src/intTest/java/com/coffee/cart/CartItemControllerIntTest.java
@@ -0,0 +1,6 @@
+package com.coffee.cart;
+
+public class CartItemControllerIntTest {
+
+    // TODO
+}
---
+++ core/src/main/java/com/coffee/cart/CartItemController.java
@@ -0,0 +1,31 @@
+package com.coffee.cart;
+
+import com.coffee.item.ItemManagementController;
+import com.coffee.publicapi.ExternalCartItemRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/cart")
+@Validated
+public class CartItemController {
+
+    private static final Logger log = LoggerFactory.getLogger(ItemManagementController.class);
+
+    @Autowired
+    private final CartItemService cartItemService;
+
+    public CartItemController(CartItemService cartItemService) {
+        this.cartItemService = cartItemService;
+    }
+
+    @PostMapping
+    public UUID addToCart(@RequestHeader("user") UUID userUid, @RequestBody ExternalCartItemRequest cartItemRequest) {
+        return cartItemService.addItemToCart(userUid, cartItemRequest);
+    }
+}
